b37e776613ff56979c7e857a4775049672da3932
Julian Paolo Dayag <i.wisdomsky@gmail.com>
2020-05-25 11:08:14 +0800

    refactor downloader

--- data_downloader/exec.js
+++ data_downloader/exec.js
@@ -1,8 +1,9 @@
 const fs = require('fs-extra');
 const rimraf = require('rimraf');
+const utils = require('./utils');
 
 
-const dest = require('./config').dest + '/';
+const config = require('./config');
 
 
 (async () => {
@@ -10,42 +11,53 @@ const dest = require('./config').dest + '/';
 
     // quests
     let quests = await require('./scripts/quest').fetch();
-    fs.writeJsonSync(dest + 'quests.json', quests, { spaces: 4});
+    fs.writeJsonSync(config.dest + '/quests.json', quests, { spaces: 4});
 
 
     // skills
     await require('./scripts/skills.icons').fetch();
     let skills = await require('./scripts/skills').fetch();
-    fs.writeJsonSync(dest + 'skills.json', skills, { spaces: 4});
+    fs.writeJsonSync(config.dest + '/skills.json', skills, { spaces: 4});
 
 
     // mobs
     let mobs = await require('./scripts/mobs').fetch();
-    fs.writeJsonSync(dest + 'mobs.json', mobs, { spaces: 4});
+    fs.writeJsonSync(config.dest + '/mobs.json', mobs, { spaces: 4});
 
 
     // items
     let items = await require('./scripts/items').fetch();
-    fs.writeJsonSync(dest + 'items.json', items, { spaces: 4});
+    fs.writeJsonSync(config.dest + '/items.json', items, { spaces: 4});
 
     let itemfiles = await require('./scripts/item.files').fetch();
-    fs.writeJsonSync(dest + 'items.files.json', itemfiles, { spaces: 4});
+    fs.writeJsonSync(config.dest + '/items.files.json', itemfiles, { spaces: 4});
 
-    let itemsmapped = await require('./scripts/item.mapped').fetch();
-    fs.writeJsonSync(dest + 'items.mapped.json', itemsmapped, { spaces: 4});
+    let itemsid = await require('./scripts/item.id').fetch();
+    fs.writeJsonSync(config.dest + '/items.id.json', itemsid, { spaces: 4});
+
+    let itemsidflipped = utils.objectFlip(itemsid);
+
+    let itemsmapped = items.map((item) => {
+        item.id = itemsidflipped[item.name];
+        return item;
+    });
+
+    fs.writeJsonSync(config.dest + '/items.mapped.json', itemsmapped, { spaces: 4});
 
 
     // bgm
     await require('./scripts/bgm').fetch();
 
+
     // gfx
     await require('./scripts/gfx').fetch();
+
 
     // gfx
     await require('./scripts/misc').fetch();
 
 
-    rimraf.sync(dest.tmp);
+    rimraf.sync(config.tmp);
     console.log('\x1b[32m%s\x1b[0m', 'Done.');
 })();
 
--- data_downloader/utils.js
+++ data_downloader/utils.js
@@ -20,3 +20,12 @@ exports.extract = function (file, dest) {
     })
 };
 
+
+
+exports.objectFlip = function(obj) {
+    const ret = {};
+    Object.keys(obj).forEach(key => {
+        ret[obj[key]] = key;
+    });
+    return ret;
+}
